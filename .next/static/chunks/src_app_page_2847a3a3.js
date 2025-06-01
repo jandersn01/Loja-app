49d394fdc1a5a5ed028a6ab78df4ed13e6fd506c
pedrolucasi <pp.pedro.sb@gmail.com>
2025-06-01 01:18:33 -0300

    add o header com  Tailwind CSS e ícones

---
+++ .next/static/chunks/src_app_page_2847a3a3.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_2847a3a3.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_2847a3a3.js",
+  "chunks": [
+    "static/chunks/src_app_components_Card_7fd41c7c.js"
+  ],
+  "source": "dynamic"
+});
